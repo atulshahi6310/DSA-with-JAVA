0757a3fd66738dd40bfd612644f29ff5dcb00d3c
atulshahi6310 <atulshahi631@gmail.com>
2025-08-30 10:22:59 +0530

    Time: 2 ms (73.81%) | Memory: 53.4 MB (21.04%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      int count = 0;
-      int repeat =0 ;
-      for(int i =0 ; i<nums.length; i++){
-        if(count == 0){
-            repeat = nums[i];   
+        int count =0;
+        int  candinate = 0;
+        for(int i =0; i<nums.length; i++){
+            if(count == 0){
+                candinate = nums[i];
+            }
+            if(nums[i] == candinate){
+                count++;
+            }else{
+                count--;
+            }
         }
-        if(nums[i] == repeat){
-            count++;
-        }else{
-            count--;
-        }
-      }
-        return  repeat;
+        return candinate;
     }
 }
