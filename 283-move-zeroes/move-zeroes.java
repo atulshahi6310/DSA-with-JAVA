ea7d68b2a63523ddcc533e982b7a2131a71fafd3
atulshahi6310 <atulshahi631@gmail.com>
2025-08-30 09:26:01 +0530

    Time: 2 ms (79.80%) | Memory: 46.2 MB (54.18%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -5,11 +5,13 @@ class Solution {
         int n = nums.length;
         for(rp =0;rp<n;rp++){
             if(nums[rp] != 0){
+                
                 int temp = nums[lp];
                 nums[lp] =nums[rp];
                 nums[rp] = temp;
                 
                 lp++;
+                
             }
         }
     }
