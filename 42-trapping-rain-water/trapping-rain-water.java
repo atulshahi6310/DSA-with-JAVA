73a40ece2247ff4e9a32ac5889279bd0178a4698
atulshahi6310 <atulshahi631@gmail.com>
2025-09-09 16:52:44 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (67.49%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-      
-        int n = height.length;
-       int[] left_max = new int[n];   
-        left_max [0] = height[0];
-        for(int i =1 ; i<n ; i++){
-            left_max[i] = Math.max(height[i],left_max[i -1]);
+        int lp = 0, rp = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int trapped_water = 0;
+
+        while (lp < rp) {
+            if (height[lp] < height[rp]) {
+                if (height[lp] >= leftMax) {
+                    leftMax = height[lp];
+                } else {
+                    trapped_water += leftMax - height[lp];
+                }
+                lp++;
+            } else {
+                if (height[rp] >= rightMax) {
+                    rightMax = height[rp];
+                } else {
+                    trapped_water += rightMax - height[rp];
+                }
+                rp--;
+            }
         }
 
-        int[] right_max = new int [n];
-        right_max [n-1] = height[n-1];
-        for(int i = n-2 ; i>=0 ;i--){
-            right_max[i] = Math.max(height[i],right_max[i + 1]);
-        }
-
-        int traped_water = 0;
-        for(int i=0; i<n ; i++){
-            int water_level = Math.min(left_max[i] , right_max[i]);
-
-            traped_water += water_level - height[i];
-        }
-
-        return traped_water;
+        return trapped_water;
     }
-} 
-    
+}
