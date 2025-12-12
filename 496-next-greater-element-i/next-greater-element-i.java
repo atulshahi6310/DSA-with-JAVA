c8b3f4dba377017f9db0c303b0f6f6612eea069e
atulshahi6310 <atulshahi631@gmail.com>
2025-12-12 11:10:49 +0530

    Time: 3 ms (88.61%) | Memory: 44.6 MB (64.47%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,33 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Deque<Integer> stack = new ArrayDeque<>();
-      
-        
-        int nums1Length = nums1.length;
-        int nums2Length = nums2.length;
-      
-        Map<Integer, Integer> nextGreaterMap = new HashMap<>(nums2Length);
-      
-        for (int i = nums2Length - 1; i >= 0; i--) {
-            int currentNum = nums2[i];
-            while (!stack.isEmpty() && stack.peek() < currentNum) {
-                stack.pop();
-            }
-          
-            if (!stack.isEmpty()) {
-                nextGreaterMap.put(currentNum, stack.peek());
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int current = nums1[i];
+            int index = -1;
+
+            
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == current) {
+                    index = j;
+                    break;
+                }
             }
 
-            stack.push(currentNum);
-        }
-      
+            int nextGreater = -1;
+            for (int k = index + 1; k < nums2.length; k++) {
+                if (nums2[k] > current) {
+                    nextGreater = nums2[k];
+                    break;
+                }
+            }
 
-        int[] result = new int[nums1Length];
-        for (int i = 0; i < nums1Length; i++) {
-           
-            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+            result[i] = nextGreater;
         }
-      
+
         return result;
     }
 }
